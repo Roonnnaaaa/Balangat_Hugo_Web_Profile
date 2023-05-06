5bf00d922201c8dd5b9f562d1ea095f0919743d1
Rona May Balangat <ronamay.balangat@bicol-u.edu.ph>
2023-05-06 21:09:42 +0800

    Changes some elements

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 //HEADER RESPONSIVE NAVBAR//
 window.onload = function () {
 	window.addEventListener('scroll', function (e) {
-		if (window.pageYOffset > 100) {
+		if (window.pageYOffset > 10) { //100//
 			document.querySelector("header").classList.add('is-scrolling');
 		} else {
 			document.querySelector("header").classList.remove('is-scrolling');
